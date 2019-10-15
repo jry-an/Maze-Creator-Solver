20ec6daf952aef859dd708ac6d78aaaa61cef50a
Jack <jackryan1998@hotmail.com>
2019-10-15 19:43:30 +1100

    edge updates

--- mazeGenerator/KruskalGenerator.java
+++ mazeGenerator/KruskalGenerator.java
@@ -34,55 +34,59 @@ public class KruskalGenerator implements MazeGenerator {
 
 			int randR = rand.nextInt(maze.sizeR);
 			int randC = rand.nextInt(maze.sizeC);
+			int randomWall = -1;
 			ArrayList<Integer> listofAvailableWalls = checkSurroundingWalls(maze,randR,randC);
-			int randomWall = listofAvailableWalls.get(rand.nextInt(listofAvailableWalls.size()));
+			if (!listofAvailableWalls.isEmpty()){
+				 randomWall = listofAvailableWalls.get(rand.nextInt(listofAvailableWalls.size()));
+			}
 
-			switch (randomWall){
-				case Maze.NORTH:	//r+1
-					if (randR < maze.sizeR) {
-						maze.map[randR][randC].wall[NORTH].present = false;
-						maze.map[randR][randC].neigh[NORTH].rootId = maze.map[randR][randC].rootId;
-						int northR = maze.map[randR][randC].neigh[NORTH].r;
-						int northC = maze.map[randR][randC].neigh[NORTH].c;
-						//TODO - recurse through neighbour tree, converting ID
-						recurseThroughTree(maze,northR,northC);
-					}
-					break;
-				case Maze.SOUTH:	//r-1
-					if (randR > 0){
-						maze.map[randR][randC].wall[SOUTH].present = false;
-						maze.map[randR][randC].neigh[SOUTH].rootId = maze.map[randR][randC].rootId;
-						int southR = maze.map[randR][randC].neigh[SOUTH].r;
-						int southC = maze.map[randR][randC].neigh[SOUTH].c;
-						//TODO - recurse through neighbour tree, converting ID
-						recurseThroughTree(maze,southR,southC);
-					}
-					break;
+			if (randomWall != -1) {
+				switch (randomWall) {
+					case Maze.NORTH:    //r+1
+						if (randR < maze.sizeR) {
+							maze.map[randR][randC].wall[NORTH].present = false;
+							maze.map[randR][randC].neigh[NORTH].rootId = maze.map[randR][randC].rootId;
+							int northR = maze.map[randR][randC].neigh[NORTH].r;
+							int northC = maze.map[randR][randC].neigh[NORTH].c;
+							//TODO - recurse through neighbour tree, converting ID
+							recurseThroughTree(maze, northR, northC);
+						}
+						break;
+					case Maze.SOUTH:    //r-1
+						if (randR > 0) {
+							maze.map[randR][randC].wall[SOUTH].present = false;
+							maze.map[randR][randC].neigh[SOUTH].rootId = maze.map[randR][randC].rootId;
+							int southR = maze.map[randR][randC].neigh[SOUTH].r;
+							int southC = maze.map[randR][randC].neigh[SOUTH].c;
+							//TODO - recurse through neighbour tree, converting ID
+							recurseThroughTree(maze, southR, southC);
+						}
+						break;
 
-				case Maze.EAST:	//c+1
-					if (randC < maze.sizeC) {
-						maze.map[randR][randC].wall[EAST].present = false;
-						maze.map[randR][randC].neigh[EAST].rootId = maze.map[randR][randC].rootId;
-						int eastR = maze.map[randR][randC].neigh[EAST].r;
-						int eastC = maze.map[randR][randC].neigh[EAST].c;
-						//TODO - recurse through neighbour tree, converting ID
-						recurseThroughTree(maze,eastR,eastC);
-					}
-					break;
+					case Maze.EAST:    //c+1
+						if (randC < maze.sizeC) {
+							maze.map[randR][randC].wall[EAST].present = false;
+							maze.map[randR][randC].neigh[EAST].rootId = maze.map[randR][randC].rootId;
+							int eastR = maze.map[randR][randC].neigh[EAST].r;
+							int eastC = maze.map[randR][randC].neigh[EAST].c;
+							//TODO - recurse through neighbour tree, converting ID
+							recurseThroughTree(maze, eastR, eastC);
+						}
+						break;
 
-				case Maze.WEST:	//c-1
-					if (randC > 0){
-						maze.map[randR][randC].wall[WEST].present = false;
-						maze.map[randR][randC].neigh[WEST].rootId = maze.map[randR][randC].rootId;
-						int westR = maze.map[randR][randC].neigh[WEST].r;
-						int westC = maze.map[randR][randC].neigh[WEST].c;
-						//TODO - recurse through neighbour tree, converting ID
-						recurseThroughTree(maze,westR,westC);
-					}
-					break;
+					case Maze.WEST:    //c-1
+						if (randC > 0) {
+							maze.map[randR][randC].wall[WEST].present = false;
+							maze.map[randR][randC].neigh[WEST].rootId = maze.map[randR][randC].rootId;
+							int westR = maze.map[randR][randC].neigh[WEST].r;
+							int westC = maze.map[randR][randC].neigh[WEST].c;
+							//TODO - recurse through neighbour tree, converting ID
+							recurseThroughTree(maze, westR, westC);
+						}
+						break;
+				}
 			}
 		}//end loop
-
 
 	} // end of generateMaze()
 
@@ -92,30 +96,36 @@ public class KruskalGenerator implements MazeGenerator {
 
 	private ArrayList<Integer> checkSurroundingWalls(Maze maze, int r, int c) {
 		ArrayList<Integer> availableDirections = new ArrayList<>();
-		if (maze.map[r][c].neigh[NORTH].rootId != maze.map[r][c].rootId){
+		if (r < maze.sizeR -1){
+		if (maze.map[r][c].neigh[NORTH].rootId != maze.map[r][c].rootId) {
 			availableDirections.add(NORTH);
+			}
 		}
-		if (maze.map[r][c].neigh[SOUTH].rootId != maze.map[r][c].rootId){
-			availableDirections.add(SOUTH);
+		if (r >0) {
+			if (maze.map[r][c].neigh[SOUTH].rootId != maze.map[r][c].rootId) {
+				availableDirections.add(SOUTH);
+			}
 		}
-		if (maze.map[r][c].neigh[EAST].rootId != maze.map[r][c].rootId){
-			availableDirections.add(EAST);
+		if (c < maze.sizeC -1) {
+			if (maze.map[r][c].neigh[EAST].rootId != maze.map[r][c].rootId) {
+				availableDirections.add(EAST);
+			}
 		}
-		if (maze.map[r][c].neigh[WEST].rootId != maze.map[r][c].rootId){
-			availableDirections.add(WEST);
+		if(c > 0) {
+			if (maze.map[r][c].neigh[WEST].rootId != maze.map[r][c].rootId) {
+				availableDirections.add(WEST);
+			}
 		}
 
 		return availableDirections;
 	}
 
 	private void initializeCellId(Maze maze){
-		int r = 0;
+		int r;
 		int c = 0;
 		int id = 0;
-		while(r < maze.sizeR && c < maze.sizeC) {
-			for (r = 0; r < maze.sizeR; r++) {
+		for (r = 0; r < maze.sizeR; r++) {
 				maze.map[r][c].rootId = id;
-			}
 			c++;
 			id++;
 		}
@@ -127,9 +137,8 @@ public class KruskalGenerator implements MazeGenerator {
 
 		int r = 0;
 		int c = 0;
-		int firstCellId = maze.map[r][c].rootId
-				;
-		while(r < maze.sizeR && c < maze.sizeC) {
+		int firstCellId = maze.map[r][c].rootId;
+		while(c < maze.sizeC) {
 			for (r = 0; r < maze.sizeR; r++) {
 				if (maze.map[r][c].rootId != firstCellId){
 					return false;
